83b2ed0794d33a0e08e0ff8097ca95e74eda67f2
[Veronika Paramonova] <[paramovv@gmail.com]>
2022-04-25 15:03:28 -0400

    edited comments

--- src/main/java/jpa/dao/CourseDAO.java
+++ src/main/java/jpa/dao/CourseDAO.java
@@ -4,7 +4,7 @@ import jpa.entitymodels.Course;
 import java.util.List;
 
 public interface CourseDAO {
-   //method declaration
+   //method declaration for courseDAO
     List<Course> getAllCourses();
     Course getCourseById(int id);
 
